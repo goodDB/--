d142ab8389d135fb427ff5f6a10abf8f03a5f8b3
goodDB <75558763+goodDB@users.noreply.github.com>
2021-01-18 15:03:36 +0800

    Add files via upload

---
+++ day-0116/pages/index/index.js
@@ -0,0 +1,136 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    images:[
+      {
+        name:'美食图片一',
+        addtime: '2020-11-12 00:00:00',
+        img: 'https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3252271869,4079591252&fm=26&gp=0.jpg'
+      },
+      {
+        name:'美食图片二',
+        addtime: '2020-11-12 00:00:00',
+        img: 'https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2595783585,714079109&fm=26&gp=0.jpg'
+      },
+      {
+        name:'美食图片三',
+        addtime: '2020-11-12 00:00:00',
+        img: 'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2450407819,2645270023&fm=26&gp=0.jpg'
+      },
+      {
+        name:'美食图片四',
+        addtime: '2020-11-12 00:00:00',
+        img: 'https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2291099658,3405744592&fm=26&gp=0.jpg'
+      },
+      {
+        name:'美食图片五',
+        addtime: '2020-11-12 00:00:00',
+        img: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3421435433,1448772455&fm=26&gp=0.jpg'
+      },
+      {
+        name:'美食图片六',
+        addtime: '2020-11-12 00:00:00',
+        img: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=320865162,3308969918&fm=26&gp=0.jpg'
+      },
+    ],
+    images2:[
+      {
+        name:'美食图片一',
+        addtime: '2020-11-12 00:00:00',
+        img: 'https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2595783585,714079109&fm=26&gp=0.jpg'
+      },
+      {
+        name:'美食图片二',
+        addtime: '2020-11-12 00:00:00',
+        img: 'https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3252271869,4079591252&fm=26&gp=0.jpg'
+      },
+      {
+        name:'美食图片三',
+        addtime: '2020-11-12 00:00:00',
+        img: 'https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2291099658,3405744592&fm=26&gp=0.jpg'
+      },
+      {
+        name:'美食图片四',
+        addtime: '2020-11-12 00:00:00',
+        img: 'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2450407819,2645270023&fm=26&gp=0.jpg'
+      },
+      {
+        name:'美食图片五',
+        addtime: '2020-11-12 00:00:00',
+        img: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3421435433,1448772455&fm=26&gp=0.jpg'
+      },
+      {
+        name:'美食图片六',
+        addtime: '2020-11-12 00:00:00',
+        img: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=320865162,3308969918&fm=26&gp=0.jpg'
+      },
+    ],
+  },
+  topTap(res){
+    wx.navigateTo({
+      url: '../video/video?id=' + res.currentTarget.id,
+    })
+  },
+  bottomTap(res){
+    console.log(res.currentTarget.id);
+    
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
---
+++ day-0116/pages/video/video.js
@@ -0,0 +1,89 @@
+function getRandomColor() {
+  const rgb = []
+  for (let i = 0; i < 3; ++i) {
+    let color = Math.floor(Math.random() * 256).toString(16)
+    color = color.length === 1 ? '0' + color : color
+    rgb.push(color)
+  }
+  return '#' + rgb.join('')
+}
+// pages/video.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    danmuValue:''
+  },
+  inputBlur(e){
+    console.log(e.detail.value);
+    this.setData({
+      danmuValue: e.detail.value
+    })
+  },
+  btnPush(){
+    this.videoContext.sendDanmu({
+      text: this.data.danmuValue,
+      color: getRandomColor()
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options.id);
+    var color = ""
+    console.log( color = color.length === 1 ? '0' + color : color);
+    console.log( color );
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    this.videoContext = wx.createVideoContext('myVideo')
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
